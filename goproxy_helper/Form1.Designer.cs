6ee245befb53b5963d44b6a11c58c4e4d0dac8e5
Isayme <week8@live.cn>
2019-12-13 17:25:11 +0800

    添加程序图标，调整默认界面字体为雅黑字体并微调界面大小和显示位置，消息对话框添加图标，服务状态颜色区分。

--- goproxy_helper/Form1.Designer.cs
+++ goproxy_helper/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             System.Windows.Forms.Button button6;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(main));
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
@@ -42,13 +43,26 @@
             button6 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
+            // button6
+            // 
+            button6.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            button6.Location = new System.Drawing.Point(426, 127);
+            button6.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            button6.Name = "button6";
+            button6.Size = new System.Drawing.Size(157, 51);
+            button6.TabIndex = 8;
+            button6.Text = "检查更新";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += new System.EventHandler(this.button6_Click);
+            // 
             // button1
             // 
-            this.button1.Font = new System.Drawing.Font("宋体", 20F, System.Drawing.FontStyle.Bold);
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.button1.ForeColor = System.Drawing.Color.Red;
-            this.button1.Location = new System.Drawing.Point(23, 41);
+            this.button1.Location = new System.Drawing.Point(27, 40);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(135, 50);
+            this.button1.Size = new System.Drawing.Size(157, 51);
             this.button1.TabIndex = 0;
             this.button1.Text = "安装服务";
             this.button1.UseVisualStyleBackColor = true;
@@ -56,11 +70,12 @@
             // 
             // button2
             // 
-            this.button2.Font = new System.Drawing.Font("宋体", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.button2.ForeColor = System.Drawing.Color.Navy;
-            this.button2.Location = new System.Drawing.Point(192, 123);
+            this.button2.Location = new System.Drawing.Point(224, 127);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(135, 50);
+            this.button2.Size = new System.Drawing.Size(157, 51);
             this.button2.TabIndex = 1;
             this.button2.Text = "打开配置";
             this.button2.UseVisualStyleBackColor = true;
@@ -68,11 +83,12 @@
             // 
             // button3
             // 
-            this.button3.Font = new System.Drawing.Font("宋体", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button3.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.button3.ForeColor = System.Drawing.Color.Red;
-            this.button3.Location = new System.Drawing.Point(191, 41);
+            this.button3.Location = new System.Drawing.Point(223, 40);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(135, 50);
+            this.button3.Size = new System.Drawing.Size(157, 51);
             this.button3.TabIndex = 2;
             this.button3.Text = "卸载服务";
             this.button3.UseVisualStyleBackColor = true;
@@ -80,11 +96,12 @@
             // 
             // button4
             // 
-            this.button4.Font = new System.Drawing.Font("宋体", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button4.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.button4.ForeColor = System.Drawing.Color.Navy;
-            this.button4.Location = new System.Drawing.Point(24, 123);
+            this.button4.Location = new System.Drawing.Point(28, 127);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(135, 50);
+            this.button4.Size = new System.Drawing.Size(157, 51);
             this.button4.TabIndex = 3;
             this.button4.Text = "打开管理";
             this.button4.UseVisualStyleBackColor = true;
@@ -93,10 +110,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label1.Location = new System.Drawing.Point(24, 228);
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(32, 224);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(96, 16);
+            this.label1.Size = new System.Drawing.Size(95, 21);
             this.label1.TabIndex = 4;
             this.label1.Text = "服务状态： ";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -104,54 +121,45 @@
             // status
             // 
             this.status.AutoSize = true;
-            this.status.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.status.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.status.ForeColor = System.Drawing.Color.Red;
-            this.status.Location = new System.Drawing.Point(116, 228);
+            this.status.Location = new System.Drawing.Point(139, 224);
             this.status.Name = "status";
-            this.status.Size = new System.Drawing.Size(88, 16);
+            this.status.Size = new System.Drawing.Size(90, 21);
             this.status.TabIndex = 5;
             this.status.Text = "服务未安装";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("宋体", 10F);
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.label2.ForeColor = System.Drawing.Color.Red;
-            this.label2.Location = new System.Drawing.Point(21, 9);
+            this.label2.Location = new System.Drawing.Point(381, 226);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(0, 14);
+            this.label2.Size = new System.Drawing.Size(0, 19);
             this.label2.TabIndex = 6;
             // 
             // button5
             // 
-            this.button5.Font = new System.Drawing.Font("宋体", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button5.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.button5.ForeColor = System.Drawing.Color.Green;
-            this.button5.Location = new System.Drawing.Point(364, 41);
+            this.button5.Location = new System.Drawing.Point(425, 40);
+            this.button5.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(135, 50);
+            this.button5.Size = new System.Drawing.Size(157, 51);
             this.button5.TabIndex = 7;
             this.button5.Text = "重启服务";
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
-            // button6
-            // 
-            button6.Font = new System.Drawing.Font("宋体", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            button6.Location = new System.Drawing.Point(365, 123);
-            button6.Name = "button6";
-            button6.Size = new System.Drawing.Size(135, 50);
-            button6.TabIndex = 8;
-            button6.Text = "检查更新";
-            button6.UseVisualStyleBackColor = true;
-            button6.Click += new System.EventHandler(this.button6_Click);
-            // 
             // button7
             // 
-            this.button7.Font = new System.Drawing.Font("宋体", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button7.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.button7.ForeColor = System.Drawing.Color.Green;
-            this.button7.Location = new System.Drawing.Point(541, 41);
+            this.button7.Location = new System.Drawing.Point(631, 40);
+            this.button7.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(135, 50);
+            this.button7.Size = new System.Drawing.Size(157, 51);
             this.button7.TabIndex = 9;
             this.button7.Text = "停止服务";
             this.button7.UseVisualStyleBackColor = true;
@@ -159,11 +167,12 @@
             // 
             // button8
             // 
-            this.button8.Font = new System.Drawing.Font("宋体", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button8.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.button8.ForeColor = System.Drawing.Color.Green;
-            this.button8.Location = new System.Drawing.Point(541, 123);
+            this.button8.Location = new System.Drawing.Point(631, 127);
+            this.button8.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(135, 50);
+            this.button8.Size = new System.Drawing.Size(157, 51);
             this.button8.TabIndex = 10;
             this.button8.Text = "启动服务";
             this.button8.UseVisualStyleBackColor = true;
@@ -171,9 +180,9 @@
             // 
             // main
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(709, 270);
+            this.ClientSize = new System.Drawing.Size(817, 277);
             this.Controls.Add(this.button8);
             this.Controls.Add(this.button7);
             this.Controls.Add(button6);
@@ -185,9 +194,14 @@
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.MaximizeBox = false;
             this.Name = "main";
-            this.Text = "goproxy控制面板小助手";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Goproxy控制面板小助手";
             this.Load += new System.EventHandler(this.main_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
--- goproxy_helper/Form1.cs
+++ goproxy_helper/Form1.cs
@@ -16,9 +16,9 @@ namespace goproxy_helper
 {
     public partial class main : Form
     {
-        private static string serviceName= "proxyadmin";
+        private static string serviceName = "proxyadmin";
         private static string binName = "proxy-admin";
-        private static string titleType = "社区版";
+        private static string titleType = "社区版-雅黑版";
         public main()
         {
             InitializeComponent();
@@ -27,11 +27,12 @@ namespace goproxy_helper
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (Service.Exists(serviceName)) {
-                MessageBox.Show("服务已经安装！");
+            if (Service.Exists(serviceName))
+            {
+                MessageBox.Show("服务已经安装！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 return;
             }
-            MessageBox.Show(exec(binName,"install",true,false,true)?"安装成功":"安装失败");
+            MessageBox.Show(exec(binName, "install", true, false, true) ? "安装成功" : "安装失败", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -50,9 +51,10 @@ namespace goproxy_helper
         }
         private string firefoxPath()
         {
-            object  path2;
-             path2 = Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\firefox.exe", "", null);
-            if (path2 != null) {
+            object path2;
+            path2 = Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\firefox.exe", "", null);
+            if (path2 != null)
+            {
                 return path2.ToString();
             }
             return "";
@@ -60,12 +62,12 @@ namespace goproxy_helper
 
         private void main_Load(object sender, EventArgs e)
         {
-           
+
             string chromebinpath = this.chromePath();
             string firefoxbinpath = this.firefoxPath();
             if (chromebinpath == "" && firefoxbinpath == "")
             {
-                label2.Text = "请先安装最新版chrome或者firefox，才能正常使用goproxy控制面板。";
+                label2.Text = "请先安装最新版chrome或者firefox，才能正常使用goproxy控制面板。\n如果已安装请手动打开使用：http://127.0.0.1:32080访问";
             }
             refreshStatus();
         }
@@ -74,12 +76,12 @@ namespace goproxy_helper
         {
             if (!Service.Exists(serviceName))
             {
-                MessageBox.Show("服务未安装！");
+                MessageBox.Show("服务未安装！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 return;
             }
             Service.Stop(serviceName);
             //exec(binName, "stop", true, false);
-            MessageBox.Show(Service.Start(serviceName) ? "重启成功" : "重启失败");
+            MessageBox.Show(Service.Start(serviceName) ? "重启成功" : "重启失败", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
         private void refreshStatus()
@@ -87,25 +89,30 @@ namespace goproxy_helper
             Thread demoThread = new Thread(new ThreadStart(threadMethod));
             demoThread.IsBackground = true;
             demoThread.Start();//启动线程 
-            
+
         }
-          void threadMethod() {
+        void threadMethod()
+        {
             string statusText = "";
-            while (true) {
+            while (true)
+            {
                 bool isExists = Service.Exists(serviceName);
                 if (isExists)
                 {
                     if (Service.IsRunning(serviceName))
                     {
+                        status.ForeColor = Color.LimeGreen;
                         statusText = "运行中";
                     }
                     else
                     {
+                        status.ForeColor = Color.Red;
                         statusText = "已停止";
                     }
                 }
                 else
                 {
+                    status.ForeColor = Color.Red;
                     statusText = "未安装";
                 }
                 Action<String> AsyncUIDelegate = delegate (string n) { status.Text = n; };
@@ -113,7 +120,8 @@ namespace goproxy_helper
                 Thread.Sleep(1000);
             }
         }
-        private bool exec(string cmd,string args,bool CreateNoWindow, bool UseShellExecute,bool wait) {
+        private bool exec(string cmd, string args, bool CreateNoWindow, bool UseShellExecute, bool wait)
+        {
             System.Diagnostics.Process p = new System.Diagnostics.Process();
             p.StartInfo.FileName = cmd;
             p.StartInfo.CreateNoWindow = CreateNoWindow;
@@ -122,13 +130,16 @@ namespace goproxy_helper
             try
             {
                 bool ok = p.Start();
-                if (wait) {
+                if (wait)
+                {
                     p.WaitForExit();
                 }
                 return ok;
             }
-            catch (Exception e) {
+            catch (Exception ex)
+            {
                 return false;
+                throw ex;
             }
         }
 
@@ -136,11 +147,11 @@ namespace goproxy_helper
         {
             if (!Service.Exists(serviceName))
             {
-                MessageBox.Show("服务未安装！");
+                MessageBox.Show("服务未安装！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 return;
             }
-            MessageBox.Show(exec(binName, "uninstall", true, false,true) ? "卸载成功" : "卸载失败");
-            
+            MessageBox.Show(exec(binName, "uninstall", true, false, true) ? "卸载成功" : "卸载失败", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -151,14 +162,14 @@ namespace goproxy_helper
                 MessageBox.Show("服务未安装！");
                 return;
             }**/
-            exec("explorer.exe", "C:\\gpa", true, false,false);
+            exec("explorer.exe", "C:\\gpa", true, false, false);
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
             if (!Service.Exists(serviceName))
             {
-                MessageBox.Show("服务未安装！");
+                MessageBox.Show("服务未安装！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 return;
             }
             string chromebinpath = this.chromePath();
@@ -173,8 +184,9 @@ namespace goproxy_helper
             {
                 browser = "firefox.exe";
             }
-            else {
-                MessageBox.Show("请先安装最新版 Google Chrome 或者 Firefox 浏览器！");
+            else
+            {
+                MessageBox.Show("请先安装最新版 Google Chrome 或者 Firefox 浏览器！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 return;
             }
             System.Diagnostics.Process.Start(browser, "http://127.0.0.1:32080");
@@ -183,37 +195,37 @@ namespace goproxy_helper
 
         private void button6_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("太难了，最近压力很大");
+            MessageBox.Show("太难了，最近压力很大", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
         private void button7_Click(object sender, EventArgs e)
         {
             if (!Service.Exists(serviceName))
             {
-                MessageBox.Show("服务未安装！");
+                MessageBox.Show("服务未安装！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 return;
             }
             if (!Service.IsRunning(serviceName))
             {
-                MessageBox.Show("服务未启动！");
+                MessageBox.Show("服务未启动！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 return;
             }
-            MessageBox.Show(Service.Stop(serviceName) ? "停止成功" : "停止失败");
+            MessageBox.Show(Service.Stop(serviceName) ? "停止成功" : "停止失败", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
         private void button8_Click(object sender, EventArgs e)
         {
             if (!Service.Exists(serviceName))
             {
-                MessageBox.Show("服务未安装！");
+                MessageBox.Show("服务未安装！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 return;
             }
             if (Service.IsRunning(serviceName))
             {
-                MessageBox.Show("服务运行中！");
+                MessageBox.Show("服务运行中！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 return;
             }
-            MessageBox.Show(Service.Start(serviceName) ? "启动成功" : "启动失败");
+            MessageBox.Show(Service.Start(serviceName) ? "启动成功" : "启动失败", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
